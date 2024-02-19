60de7db28bc08de14d638a9d3012c44a09c9888c
eduard96c <ciubaneduard1196@gmail.com>
2024-02-19 11:35:10 -0800

    loading data from api

--- src/index.js
+++ src/index.js
@@ -1,2 +1,14 @@
 import "./style.css";
-console.log("test");
+
+function load_teams() {
+  const promise = fetch("http://localhost:3000/teams-json")
+    .then(res => res.json())
+    .then(data => {
+      console.table(data);
+      return data;
+    });
+
+  console.log(promise);
+}
+
+load_teams();
