511b4ed5ffdf8a903bdcc87b2f26244bc523c0bb
eduard96c <ciubaneduard1196@gmail.com>
2024-02-26 10:21:19 -0800

    readme + get team as html  function

--- src/index.js
+++ src/index.js
@@ -1,32 +1,31 @@
 import "./style.css";
 let teams;
-
-async function load_teams() {
-  const promise = await fetch("http://localhost:3000/teams-json")
+function load_teams() {
+  const promise = fetch("http://localhost:3000/teams-json")
     .then(res => res.json())
     .then(data => {
-      teams = data;
+      display_teams(data);
     });
 }
 
-function display_teams() {
+function get_team_as_html(team) {
+  return `<tr>
+  <td>${team.promotion}</td>
+  <td>${team.members}</td>
+  <td>${team.name}</td>
+  <td><a target="_blank" href="${team.url}">View</a></td>
+  <td></td>
+  </tr>
+`;
+}
+function display_teams(teams) {
   if (!teams) {
     return false;
   }
-  let html = "";
-  teams.forEach(team => {
-    html += `<tr>
-            <td>${team.promotion}</td>
-            <td>${team.members}</td>
-            <td>${team.name}</td>
-            <td><a target="_blank" href="${team.url}">View</a></td>
-            <td></td>
-            </tr>
-    `;
-  });
-  console.log(teams);
-  document.querySelector("tbody").innerHTML = html;
+
+  let html = teams.map(team => get_team_as_html(team));
+
+  document.querySelector("tbody").innerHTML = html.join("");
 }
 
-await load_teams();
-display_teams();
+load_teams();
